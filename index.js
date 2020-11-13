45e1b7ca3f9b43b07b149ce68fa73e6bc65335b1
Taylor Keazirian <taylork26@gmail.com>
2020-11-13 09:26:03 -0600

    Added JS files, pseudocode, initial changes

---
+++ index.js
@@ -0,0 +1,71 @@
+
+
+$(function() {
+    $("button[class='shopping-item-delete']").on("click", "li", function(event) {
+        this.remove();
+    });
+});
+
+
+/*
+$( "button[class='shopping-item-delete']").click(function(event) {
+    this.$( ".shopping-item" ).remove();
+  });
+
+
+
+
+// CHECK:
+// toggle button to use class "shopping-item_checked"
+// target shopping item 
+
+
+$(function() {
+    $(".shopping-item-toggle").click(() => {
+        $(".shopping-item").toggleClass(".shopping-item_checked"); 
+    });
+});
+
+$(function() {
+    $(".shopping-item-toggle").click(function(event) {
+        $(".shopping-item").toggleClass(".shopping-item_checked");
+    });
+});
+
+$(function() {
+    $("button").on('click', '.shopping-item-toggle', function(event) {
+        this.toggleClass(".shopping-item_checked");
+    });
+});
+
+// ADD:
+// need to add an <li> into <ul class="shopping-list"
+// <li> contains the <span class="shopping-item" and <div class="shopping-item-controls"
+// Need to be able to add the whole <li> and all it contains
+// run a function that will append the item to the list
+// shopping-list-entry needs to go into shopping-list (the selector expression preceding the method is the container into which the content is inserted)
+
+$(function() {
+    $("#js-shopping-list-form").submit(event => {
+        event.preventDefault();
+        let newEntry = $("#shopping-list-entry").val();
+        $("#shopping-list").append($('<li>').append(newEntry, ".shopping-item-controls"));
+        $('#shopping-list-entry').val('');
+    });
+});
+
+
+// DELETE:
+// remove.() the whole <li> and all the contents
+// when delete button is clicked, trigger event to .remove() the <li>
+
+$(function() {
+    $('.shopping-item-delete').on("click", "ul", function(event) {
+        this.remove();
+    });
+});
+*/
+
+
+
+
