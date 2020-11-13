531206ff7d3d56cedddcc4aabf97495e9b9dfd39
Taylor Keazirian <taylork26@gmail.com>
2020-11-13 13:22:36 -0600

    Updated JS functions - all working!

--- index.js
+++ index.js
@@ -1,57 +1,16 @@
-
-
-
-// DELETE:
-// remove.() the whole <li> and all the contents
-// when delete button is clicked, trigger event to .remove() the <li>
-
-// $(function() {
-//     $('.shopping-item-delete').on("click", "ul", function(event) {
-//         this.remove();
-//     });
-// });
-// */
-
 $(function() {
-    $("button[class='shopping-item-delete']").on("click", function(event) {
-        $('.shopping-item').closest('li').remove();
+
+// DELETE button function
+
+    $(".shopping-list").on("click", ".shopping-item-delete", function(event) {
+        this.closest('li').remove();
     });
-});
 
-console.log("YOUR NAME");
+// CHECK button function - toggle class
 
-/*
-$( "button[class='shopping-item-delete']").click(function(event) {
-    this.$( ".shopping-item" ).remove();
-  });
-  -- closest() ??
-  -- event.which ??
-
-
-
-
-// CHECK:
-// toggle button to use class "shopping-item_checked"
-// target shopping item 
-
-
-$(function() {
-    $(".shopping-item-toggle").click(() => {
-        $(".shopping-item").toggleClass(".shopping-item_checked"); 
+    $(".shopping-list").on("click", ".shopping-item-toggle", function() {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
     });
-});
-
-$(function() {
-    $(".shopping-item-toggle").click(function(event) {
-        $(".shopping-item").toggleClass(".shopping-item_checked");
-    });
-});
-
-$(function() {
-    $("button").on('click', '.shopping-item-toggle', function(event) {
-        this.toggleClass(".shopping-item_checked");
-    });
-});
 
 // ADD:
 // need to add an <li> into <ul class="shopping-list"
@@ -60,16 +19,29 @@ $(function() {
 // run a function that will append the item to the list
 // shopping-list-entry needs to go into shopping-list (the selector expression preceding the method is the container into which the content is inserted)
 
-$(function() {
-    $("#js-shopping-list-form").submit(event => {
-        event.preventDefault();
-        let newEntry = $("#shopping-list-entry").val();
-        $("#shopping-list").append($('<li>').append(newEntry, ".shopping-item-controls"));
-        $('#shopping-list-entry').val('');
-    });
+
+$("#js-shopping-list-form").submit(event => {
+    event.preventDefault();
+    let newEntry = $("#shopping-list-entry").val();
+    $(".shopping-list").append(`<li>
+    <span class="shopping-item">${newEntry}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`);
+  $('#shopping-list-entry').val('');
 });
 
-*/
+});
+
+
+
+
 
 
 
